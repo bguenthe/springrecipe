382587c5628270b93990ed4c9adcb1e3de2c657d
bguenthe <bernd.stuebe@gmail.com>
2018-03-04 15:55:42 +0100

    use postgres

---
+++ src/main/java/de/bguenthe/springrecipe/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -0,0 +1,29 @@
+package de.bguenthe.springrecipe.converters;
+
+import de.bguenthe.springrecipe.commands.UnitOfMeasureCommand;
+import de.bguenthe.springrecipe.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
+}
